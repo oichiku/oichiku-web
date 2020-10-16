046669db3823c170319a4dd925b62dad0a78028b
tomokiz <tomoki.cfr3.0628@gmail.com>
2020-10-17 08:27:44 +0900

    change framework to fastapi

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
-import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-  return render_template('index.html')
-
-if __name__ == "__main__":
-  os.environ['PORT']
-  app.run(debug=False, host='0.0.0.0', port=80)
+from fastapi import FastAPI, Request
+  from fastapi.responses import HTMLResponse
+  from fastapi.staticfiles import StaticFiles
+  from fastapi.templating import Jinja2Templates
+  
+  app = FastAPI()
+  
+  app.mount("/static", StaticFiles(directory="static"), name="static")
+  
+  
+  templates = Jinja2Templates(directory="templates")
+  
+  
+  @app.get("/", response_class=HTMLResponse)
+  async def read_item(request: Request):
+      return templates.TemplateResponse("index.html", {"request": request})
