eb1fe861e58506c6bc32bd9d7f593b9bb34442bd
Tomoki Takahashi <tomoki.cfr3.0628@gmail.com>
2020-11-06 22:57:44 +0900

    add import subprocess to main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import subprocess
 
 app = FastAPI()
 
