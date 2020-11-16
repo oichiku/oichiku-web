6bf49f03a0d5a9c224346353615a478fa5ba3d2e
kaithash <dev.tomokiz@gmail.com>
2020-11-16 09:57:38 +0900

    change path of oichiku-deploy

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def read_item(request: Request):
 
 @app.post("/webhook")
 async def webhook():
-    res = subprocess.check_output("oichiku-deploy")
+    res = subprocess.check_output("/root/oichiku-deploy")
     return res
 
 @app.get("/{path_param}", response_class=HTMLResponse)
