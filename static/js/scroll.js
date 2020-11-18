2ef6eb20f902442b3da265d77125f4726a35e7ff
kaithash <dev.tomokiz@gmail.com>
2020-11-18 15:46:18 +0900

    change js name

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,4 +1,11 @@
 $(window).scroll(function() {
+    var distanceTop = 50;
+    if ($(window).scrollTop() > distanceTop) {
+        $('div.pullup').fadeOut();
+    } else {
+        $('div.pullup').fadeIn("fast");
+    }
+
     if ($(window).scrollTop() > $(window).height()) {
         $('header').addClass('backgroundOn')
     } else {
