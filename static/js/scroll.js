d170016ba66162523da4986470bc0a95d89a55f9
kaithash <dev.tomokiz@gmail.com>
2020-11-18 18:10:50 +0900

    hide oideyo when scrolled

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -8,7 +8,9 @@ $(window).scroll(function() {
 
     if ($(window).scrollTop() > $(window).height()) {
         $('header').addClass('backgroundOn');
+        $('.oideyo').hide();
     } else {
         $('header').removeClass('backgroundOn');
+        $('.oideyo').show();
     }
 })
