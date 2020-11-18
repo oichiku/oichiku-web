0672c3806031b559f6d81229eeeed957dc068d20
kaithash <dev.tomokiz@gmail.com>
2020-11-19 08:41:34 +0900

    change scroll button

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,11 +1,4 @@
 $(window).scroll(function() {
-    var distanceTop = 50;
-    if ($(window).scrollTop() > distanceTop) {
-        $('div.pullup').fadeOut();
-    } else {
-        $('div.pullup').fadeIn("fast");
-    }
-
     if ($(window).scrollTop() > $(window).height()) {
         $('header').addClass('backgroundOn');
         $('.oideyo').hide();
