807426f79a1a3ae8e36a6ff021db662011b4e194
Laddge <dev.laddge@gmail.com>
2021-03-10 14:06:45 +0900

    remove article section in main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing import Optional
-import pandas as pd
 import subprocess
 import os
 import random
@@ -30,34 +29,6 @@ async def index(request: Request):
     index_html = templates.get_template('top.html').render({"version": version, "bgimage": bgimg})
     footer = templates.get_template('footer.html').render({"version": version})
     return header + index_html + footer
-
-
-@app.get("/article", response_class=HTMLResponse)
-async def article(request: Request):
-    bgimg = os.listdir("/usr/share/oichiku/background")
-    bgimg = bgimg[random.randint(0, len(bgimg) - 1)]
-    version = subprocess.check_output(
-        ["git", "rev-parse", "--short", "HEAD"], text=True)
-    return templates.TemplateResponse("article.html", {"request": request, "bgimage": bgimg, "version": version})
-
-
-@app.get("/article/view", response_class=HTMLResponse)
-async def article_view(request: Request, id: Optional[str] = None):
-    csv = pd.read_csv("/usr/share/list.csv", encoding="utf_8", dtype=object)
-    csv = csv[csv.isenable == "1"]
-    if (csv.id == id).sum() == 0:
-        return templates.TemplateResponse("404.html", {"request": request})
-    bgimg = os.listdir("/usr/share/oichiku/background")
-    bgimg = bgimg[random.randint(0, len(bgimg) - 1)]
-    version = subprocess.check_output(
-        ["git", "rev-parse", "--short", "HEAD"], text=True)
-    with open("/usr/share/oichiku/html/" + id + ".html", "r") as f:
-        content = f.read()
-    content = "<style>noarticle{display:none;}server{display:inline;}</style>\n" + content
-    title = csv.title[csv.id == id].iloc[-1]
-    created = csv.created[csv.id == id].iloc[-1]
-    created = opdate(created)
-    return templates.TemplateResponse("article-view.html", {"request": request, "bgimage": bgimg, "version": version, "title": title, "created": created, "content": content})
 
 
 @app.post("/webhook", response_class=HTMLResponse)
