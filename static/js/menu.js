e536be8d5f30981a4382bea33337f7eeef4ce43e
Laddge <dev.laddge@gmail.com>
2021-03-10 21:14:15 +0900

    update design

--- static/js/menu.js
+++ static/js/menu.js
@@ -2,10 +2,8 @@ $(function() {
     $('#menu-checkbox').click(function() {
         if ($(this).prop('checked') == true) {
             $('.menu-button').addClass('is-active');
-            $('body').css('overflow', 'hidden');
         } else {
             $('.menu-button').removeClass('is-active');
-            $('body').css('overflow', 'scroll');
         }
     });
 });
--- static/js/scroll.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-    if ($(window).scrollTop() > $(window).height()) {
-        $('header').addClass('backgroundOn');
-        $('.oideyo').hide();
-    } else {
-        $('header').removeClass('backgroundOn');
-        $('.oideyo').show();
-    }
-})
