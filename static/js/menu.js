6293aefca68bdb7e73e497dbf72bcda7e2ef8cb3
Laddge <dev.laddge@gmail.com>
2021-03-11 09:48:38 +0900

    rm menu.js

--- static/js/menu.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('#menu-checkbox').click(function() {
-        if ($(this).prop('checked') == true) {
-            $('.menu-button').addClass('is-active');
-        } else {
-            $('.menu-button').removeClass('is-active');
-        }
-    });
-});
