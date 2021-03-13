4173f9a7d65d59b5b5b17343fb21f248c98be150
Tomoki Takahashi <dev.laddge@gmail.com>
2021-03-13 22:10:22 +0900

    add image to top

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def index(request: Request):
     version = subprocess.check_output(
         ["git", "rev-parse", "--short", "HEAD"], text=True)
     header = templates.get_template('header.html').render({"version": version})
-    index_html = templates.get_template('top.html').render({"version": version, "bgimage": bgimg})
+    index_html = templates.get_template('top.html').render({"version": version, "bgimg": bgimg})
     footer = templates.get_template('footer.html').render({"version": version})
     return header + index_html + footer
 
